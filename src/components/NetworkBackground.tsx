eaf754c8d552696192aa8870d8fc813573e48701
Erwin-Charles <erwinchraoul17@gmail.com>
2026-01-24 13:09:06 +0100

    Correct errors and replace gallery images with people of color as requested

--- src/components/NetworkBackground.tsx
+++ src/components/NetworkBackground.tsx
@@ -4,7 +4,7 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
 const NetworkBackground = () => {
-    const particlesInit = useCallback(async (engine: Engine) => {
+    const particlesInit = useCallback(async (engine: any) => {
         await loadFull(engine);
     }, []);
 
